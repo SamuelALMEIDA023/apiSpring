a298b4cef7bd33553481598e818c78c2353916e3
SamuelALMEIDA023 <sa_muelmarcos@hotmail.com>
2025-02-07 12:23:28 -0300

    Product-OrderItem one-to-many association

--- study/src/main/java/com/samdev/study/repositories/OrderItemRepository.java
+++ study/src/main/java/com/samdev/study/repositories/OrderItemRepository.java
@@ -2,8 +2,9 @@ package com.samdev.study.repositories;
 
 import com.samdev.study.entities.Category;
 import com.samdev.study.entities.OrderItem;
+import com.samdev.study.entities.pk.OrderItemPk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPk> {
 
 }
