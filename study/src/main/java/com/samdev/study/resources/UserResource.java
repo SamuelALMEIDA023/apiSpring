7d4f570084293bc3a71ab77f1be5bc3482e1eef0
SamuelALMEIDA023 <sa_muelmarcos@hotmail.com>
2025-02-12 10:39:14 -0300

    User insert

--- study/src/main/java/com/samdev/study/resources/UserResource.java
+++ study/src/main/java/com/samdev/study/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.samdev.study.entities.User;
 import com.samdev.study.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -19,7 +18,7 @@ public class UserResource {
     @Autowired
     private UserService service;
     //para testar se está funcionando,
-    // crio um metodo que sera um endpoint p/ acessar os usuarios
+    // crio um metodo que sera um endpoint p/ acessar os usuarios (Recuperar dados do BD)
     @GetMapping // mostrar que quero fazer uma requisicao GET (chamar)
     public ResponseEntity<List<User>> findAll() {
         List<User> list = service.findAll();
@@ -30,5 +29,15 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    //Inserindo novo recurso ao BD
+    @PostMapping
+    // metodo Response com a anotattion @RequestBody que pega JSON da requisição e tranforma para obj User
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        User newUser = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(newUser.getId()).toUri();
+        return ResponseEntity.created(uri).body(newUser);
     }
 }
